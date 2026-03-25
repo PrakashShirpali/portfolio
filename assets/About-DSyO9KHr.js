4e9c9556f90d85476826138d0b88d2c475a2dd9e
Prakashgslsa25 <shirpali1999@gmail.com>
2026-03-26 00:37:14 +0530

    Updates

--- assets/About-DSyO9KHr.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DT_hYVwX.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
--- assets/Contact-C_zWmcGk.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DT_hYVwX.js";const r=()=>t.jsx("div",{children:"Contact"});export{r as default};
